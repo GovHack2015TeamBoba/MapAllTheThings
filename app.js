86188896b5cbfe625d9ef72ad30a80314dc576e7
Marcus Holmes <mh@marcusholmes.biz>
2015-07-03 23:50:55 +0800

    set up default views

--- app.js
+++ app.js
@@ -8,11 +8,19 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+var nunjucks = require('nunjucks');
+
 var app = express();
+
+//nunjucks config
+nunjucks.configure('views',{
+  autoescape:true,
+  express:app
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Make All The Things!' });
 });
 
 module.exports = router;
