6a8b96b23a07c26017fb49215c5e36a20ac7fd29
Patrik Affentranger <patrik.affentranger@gmail.com>
2015-07-04 11:10:58 +0800

    Set up and use gulp to generate all the things

---
+++ gulpfile.js
@@ -0,0 +1,62 @@
+"use strict";
+
+var gulp          = require('gulp');
+var browserSync   = require('browser-sync').create();
+var sass          = require('gulp-sass');
+var plumber       = require('gulp-plumber');
+var autoprefixer  = require('gulp-autoprefixer');
+var include       = require('gulp-include');
+
+// all the paths------------------------------------------------------------------------------------
+
+var stylesheets = 'src/stylesheets';
+var javascripts = 'src/javascripts';
+
+var paths = {
+  styles: stylesheets,
+  stylesheet:  stylesheets + '/styles.{scss,sass}',
+  stylesheets: stylesheets + '/**/*.{scss,sass}',
+  javascript:  javascripts + '/application.js',
+  javascripts: javascripts + '/**/*.js',
+};
+
+gulp.task('js', function () {
+    return gulp.src(paths.javascript)
+        .pipe(plumber())
+        .pipe(include())
+        .pipe(gulp.dest('public'));
+});
+gulp.task('js-watch', ['js'], browserSync.reload);
+
+
+gulp.task('sass', function() {
+    return gulp.src(paths.stylesheets)
+        .pipe(plumber())
+        .pipe(sass({
+            indentedSyntax: true,
+            includePaths: paths.styles,
+            imagePath: 'img',
+            outputStyle: 'expanded'
+        }))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest("public"))
+        .pipe(browserSync.stream());
+});
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass', 'js'], function() {
+
+    browserSync.init(null, {
+        proxy: "http://localhost:3000",
+        open: false
+    });
+
+    gulp.watch(paths.stylesheets, ['sass']);
+    gulp.watch(paths.javascripts, ['js-watch']);
+    gulp.watch("views/**/*.html").on('change', browserSync.reload);
+});
+
+gulp.task('default', ['serve']);
---
+++ src/javascripts/application.js
@@ -0,0 +1,5 @@
+//=require vendor/jquery.js
+
+$(document).ready(function () {
+  // do all the things
+});
