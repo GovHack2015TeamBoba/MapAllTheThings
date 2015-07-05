e8fa8a96fc557e775b2df399d2c989937903daee
MaryHewitt <mary.hewitt@postgrad.curtin.edu.au>
2015-07-05 13:55:17 +0800

    Update slip_data.js

--- src/javascripts/data/slip_data.js
+++ src/javascripts/data/slip_data.js
@@ -1,27 +1,27 @@
 MapAllTheThings.slip_layers = [
   {
-    "name": "Where can Garden Bores be installed?",
-    "slipName": "Garden Bore Suitability",
-    "snake_case": "garden_bore_suitability",
-    "assetId": "09372590152434720789-01302480197074991316",
+    "name": "Who lives here?",
+    "slipName": "Selected Person Characteristics (LGA) ABS B01",
+    "snake_case": "selected_person_characteristics_LGA_ABS_B01",
+    "assetId": "09372590152434720789-12830528591563949894",
     "mapORlayer": "layer",
     "works": true,
     "dataType": "Polygon"
   },
   {
-    "name": "Public Transport Routes - Bus & Train",
-    "slipName": "Public Transport Authority Services - Bus Routes",
-    "snake_case": "public_transport_authority_services_bus_routes",
-    "assetId": "09372590152434720789-13313542664337428076",
+    "name": "Electorates",
+    "slipName": "State Electorates (Current)",
+    "snake_case": "state_electorates",
+    "assetId": "09372590152434720789-07805245591538660512",
     "mapORlayer": "layer",
     "works": true,
-    "dataType": "Line"
+    "dataType": "Polygon"
   },
   {
-    "name": "Public Transport Stops - Bus & Train",
-    "slipName": "Public Transport Authority Services - Bus Stops",
-    "snake_case": "public_transport_authority_services_bus_stops",
-    "assetId": "09372590152434720789-02406381474707693508",
+    "name": "Fire Stations",
+    "slipName": "DFES Stations",
+    "snake_case": "dfes_stations",
+    "assetId": "09372590152434720789-16346722722399592455",
     "mapORlayer": "layer",
     "works": true,
     "dataType": "Point"
@@ -36,6 +36,51 @@ MapAllTheThings.slip_layers = [
     "dataType": "Polygon"
   },
   {
+    "name": "Environmentally Sensitive Areas",
+    "slipName": "Environmentally Sensitive Areas",
+    "snake_case": "environmental_sensitivity",
+    "assetId": "09372590152434720789-01627525817521224070",
+    "mapORlayer": "layer",
+    "works": true,
+    "dataType": "Polygon"
+  },
+  {
+    "name": "Public Transport Routes",
+    "slipName": "Public Transport Authority Services - Bus Routes",
+    "snake_case": "public_transport_authority_services_bus_routes",
+    "assetId": "09372590152434720789-13313542664337428076",
+    "mapORlayer": "layer",
+    "works": true,
+    "dataType": "Line"
+  },
+  {
+    "name": "Public Transport Stops",
+    "slipName": "Public Transport Authority Services - Bus Stops",
+    "snake_case": "public_transport_authority_services_bus_stops",
+    "assetId": "09372590152434720789-02406381474707693508",
+    "mapORlayer": "layer",
+    "works": true,
+    "dataType": "Point"
+  },
+  {
+    "name": "Railway Tracks",
+    "slipName": "Railway Lines",
+    "snake_case": "railway_lines",
+    "assetId": "09372590152434720789-18334593804525489987",
+    "mapORlayer": "layer",
+    "works": true,
+    "dataType": "Line"
+  },
+  {
+    "name": "Bicycle Routes",
+    "slipName": "Bicycle Routes and Shared Paths",
+    "snake_case": "bicycle_routes_and_shared_paths",
+    "assetId": "09372590152434720789-14689280532638025030",
+    "mapORlayer": "layer",
+    "works": true,
+    "dataType": "Line"
+  },
+  {
     "name": "Speed Limits",
     "slipName": "Speed Data MRWA",
     "snake_case": "speed_data_mrwa",
@@ -43,15 +88,6 @@ MapAllTheThings.slip_layers = [
     "mapORlayer": "layer",
     "works": true,
     "dataType": "Line"
-  },
-  {
-    "name": "Electorates",
-    "slipName": "State Electorates (Current)",
-    "snake_case": "state_electorates",
-    "assetId": "09372590152434720789-07805245591538660512",
-    "mapORlayer": "layer",
-    "works": true,
-    "dataType": "Polygon"
   },
   {
     "name": "Future Roads",
@@ -63,37 +99,10 @@ MapAllTheThings.slip_layers = [
     "dataType": "Line"
   },
   {
-    "name": "Water Demand Areas",
-    "slipName": "Water Demand Areas",
-    "snake_case": "water_demand_region_boundaries",
-    "assetId": "09372590152434720789-01064970710144667276",
-    "mapORlayer": "map",
-    "works": true,
-    "dataType": "Polygon"
-  },
-  {
     "name": "Aboriginal Communities",
     "slipName": "Aboriginal Communities in WA",
     "snake_case": "aboriginal_communities_in_wa",
     "assetId": "09372590152434720789-08518116787816413147",
-    "mapORlayer": "layer",
-    "works": true,
-    "dataType": "Point"
-  },
-  {
-    "name": "I Want To Ride My Bicycle",
-    "slipName": "Bicycle Routes and Shared Paths",
-    "snake_case": "bicycle_routes_and_shared_paths",
-    "assetId": "09372590152434720789-14689280532638025030",
-    "mapORlayer": "layer",
-    "works": true,
-    "dataType": "Line"
-  },
-  {
-    "name": "Fire Stations",
-    "slipName": "DFES Stations",
-    "snake_case": "dfes_stations",
-    "assetId": "09372590152434720789-16346722722399592455",
     "mapORlayer": "layer",
     "works": true,
     "dataType": "Point"
@@ -108,7 +117,16 @@ MapAllTheThings.slip_layers = [
     "dataType": "Polygon"
   },
   {
-    "name": "Where does our Water Come from?",
+    "name": "Areas of Water Demand",
+    "slipName": "Water Demand Areas",
+    "snake_case": "water_demand_region_boundaries",
+    "assetId": "09372590152434720789-01064970710144667276",
+    "mapORlayer": "map",
+    "works": true,
+    "dataType": "Polygon"
+  },
+  {
+    "name": "Where does our Drinking Water Come from?",
     "slipName": "Public Drinking Water Source Areas",
     "snake_case": "public_drinking_water_source",
     "assetId": "09372590152434720789-10740193452754088647",
@@ -117,7 +135,7 @@ MapAllTheThings.slip_layers = [
     "dataType": "Polygon"
   },
   {
-    "name": "Where does it flood?",
+    "name": "Where does it Flood?",
     "slipName": "FPM Flood Level Contours (mAHD)",
     "snake_case": "flood_level_contours",
     "assetId": "09372590152434720789-18350116626117757948",
@@ -126,37 +144,19 @@ MapAllTheThings.slip_layers = [
     "dataType": "Polygon"
   },
   {
-    "name": "Railway Tracks",
-    "slipName": "Railway Lines",
-    "snake_case": "railway_lines",
-    "assetId": "09372590152434720789-18334593804525489987",
+    "name": "Where can Garden Bores be installed?",
+    "slipName": "Garden Bore Suitability",
+    "snake_case": "garden_bore_suitability",
+    "assetId": "09372590152434720789-01302480197074991316",
     "mapORlayer": "layer",
     "works": true,
-    "dataType": "Line"
+    "dataType": "Polygon"
   },
   {
-    "name": "Will your Bore Water stain your garden red?*",
+    "name": "Will your Bore Water stain your garden red?",
     "slipName": "Iron Staining Risk",
     "snake_case": "iron_staining_risk",
     "assetId": "09372590152434720789-01595251729696585091",
-    "mapORlayer": "layer",
-    "works": true,
-    "dataType": "Polygon"
-  },
-  {
-    "name": "How fragile is your ecosystem?",
-    "slipName": "Environmentally Sensitive Areas",
-    "snake_case": "environmental_sensitivity",
-    "assetId": "09372590152434720789-01627525817521224070",
-    "mapORlayer": "layer",
-    "works": true,
-    "dataType": "Polygon"
-  },
-  {
-    "name": "Who lives here?",
-    "slipName": "Selected Person Characteristics (LGA) ABS B01",
-    "snake_case": "selected_person_characteristics_LGA_ABS_B01",
-    "assetId": "09372590152434720789-12830528591563949894",
     "mapORlayer": "layer",
     "works": true,
     "dataType": "Polygon"
