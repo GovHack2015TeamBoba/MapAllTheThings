57dd1e5cde9e86ccd23550778fca926f22601150
Marcus Holmes <mh@marcusholmes.biz>
2015-07-05 16:12:20 +0800

    about all the people

--- routes/index.js
+++ routes/index.js
@@ -1,79 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var data =[
-    {
-      id:1,
-      title:"Map All the things",
-      parent:0,
-      url:""
-    },
-    {
-      id:2,
-      title:"Science",
-      parent:0,
-      url:""
-    },
-    {
-      id:3,
-      title:"Civics",
-      parent:0,
-      url:""
-    },
-    {
-      id:4,
-      title:"Other",
-      parent:0,
-      url:""
-    },
-    {
-      id:5,
-      title:"Different",
-      parent:0,
-      url:""
-    },
-    {
-      id:6,
-      title:"Foo",
-      parent:1,
-      url:""
-    },
-    {
-      id:7,
-      title:"Bar",
-      parent:2,
-      url:""
-    },
-    {
-      id:8,
-      title:"Baz",
-      parent:7,
-      url:""
-    },
-    {
-      id:9,
-      title:"Wibble",
-      parent:3,
-      url:""
-    },
-    {
-      id:10,
-      title:"Frobble",
-      parent:3,
-      url:""
-    },
-    {
-      id:11,
-      title:"Frodo",
-      parent:4,
-      url:""
-    }
-  ];
-  var rd = {title:"Map All the Things", wizard:JSON.stringify(data)};
-  res.render('index', rd);
+  res.render('index', {});
 });
 
+router.get('/about', function(req,res,next){
+  res.render('about',{});
+});
 module.exports = router;
